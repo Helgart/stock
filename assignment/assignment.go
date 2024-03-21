cb7de3c87b711599266460710dbd14d105bc86ec
Michael FORASTE <michael.foraste@gmail.com>
2024-03-21 21:51:45 +0100

    Add error handling for insufficient quantity in assignments
    A new error type, NotEnoughQuantity, has been added to manage cases where uptake requests exceed the available quantity in an assignment. The function Uptake() now returns this error when there isn't enough quantity. Parallely, added new tests to verify the new functionality, checking scenarios wherein the uptake quantity exceeds the available quantity.

--- assignment/assignment.go
+++ assignment/assignment.go
@@ -1,8 +1,11 @@
 package assignment
 
 import (
+	"fmt"
 	"stock.ngmengineering.fr/stock/item"
 )
+
+const notEnoughQuantityErrorMessage = "Not enough quantity. Available: %d, Wanted: %d"
 
 type Unit string
 type Quantity uint
@@ -11,7 +14,16 @@ type Assignment struct {
 	Item     item.Item
 	Unit     Unit
 	Quantity Quantity
-	Uptake   []Uptake
+	Uptakes  []Uptake
+}
+
+type NotEnoughQuantity struct {
+	Available Quantity
+	Wanted    Quantity
+}
+
+func (notEnoughQuantity NotEnoughQuantity) Error() string {
+	return fmt.Sprintf(notEnoughQuantityErrorMessage, notEnoughQuantity.Available, notEnoughQuantity.Wanted)
 }
 
 func NewAssignment(
@@ -29,9 +41,21 @@ func NewAssignment(
 func (assignment *Assignment) GetAvailableQuantity() Quantity {
 	availableQuantity := assignment.Quantity
 
-	for _, uptake := range assignment.Uptake {
+	for _, uptake := range assignment.Uptakes {
 		availableQuantity -= uptake.Quantity
 	}
 
 	return availableQuantity
+}
+
+func (assignment *Assignment) Uptake(quantity Quantity) (Quantity, error) {
+	availableQuantity := assignment.GetAvailableQuantity()
+
+	if availableQuantity < quantity {
+		return 0, NotEnoughQuantity{availableQuantity, quantity}
+	}
+
+	assignment.Uptakes = append(assignment.Uptakes, NewUptake(quantity))
+
+	return assignment.GetAvailableQuantity(), nil
 }
--- assignment/assignment_test.go
+++ assignment/assignment_test.go
@@ -36,3 +36,78 @@ func TestAssignmentCreation(t *testing.T) {
 		}
 	}
 }
+
+func TestAssignment_GetAvailableQuantity(t *testing.T) {
+	tests := []struct {
+		itemName string
+		unit     Unit
+		quantity Quantity
+		uptake   Quantity
+	}{
+		{"pasta", "box", 1, 1},
+		{"rice", "gram", 500, 250},
+	}
+
+	for _, fixture := range tests {
+		itemToAssign, err := item.NewItem(fixture.itemName)
+
+		if err != nil {
+			t.Errorf("Error creating item: %v", err)
+			continue
+		}
+
+		itemAssignment := NewAssignment(itemToAssign, fixture.unit, fixture.quantity)
+		itemUptake := NewUptake(fixture.uptake)
+
+		itemAssignment.Uptakes = append(itemAssignment.Uptakes, itemUptake)
+
+		currentQuantity := itemAssignment.GetAvailableQuantity()
+		expectedQuantity := fixture.quantity - fixture.uptake
+
+		if currentQuantity != fixture.quantity-fixture.uptake {
+			t.Errorf("Expected %d quantity after uptake, obtained %d", expectedQuantity, currentQuantity)
+		}
+	}
+}
+
+func TestAssignment_Uptake(t *testing.T) {
+	tests := []struct {
+		itemName string
+		unit     Unit
+		quantity Quantity
+		uptake   Quantity
+	}{
+		{"pasta", "box", 1, 1},
+		{"rice", "gram", 500, 250},
+		{"floor", "gram", 1000, 1200},
+	}
+
+	for _, fixture := range tests {
+		itemToAssign, err := item.NewItem(fixture.itemName)
+
+		if err != nil {
+			t.Errorf("Error creating item: %v", err)
+			continue
+		}
+
+		itemAssignment := NewAssignment(itemToAssign, fixture.unit, fixture.quantity)
+
+		currentQuantity, err := itemAssignment.Uptake(fixture.uptake)
+
+		if fixture.uptake > fixture.quantity && err == nil {
+			t.Errorf("Expected error after uptake, none obtained")
+		}
+		if fixture.uptake > fixture.quantity && err != nil {
+			if _, isValid := err.(NotEnoughQuantity); !isValid {
+				t.Errorf("Expected NotEnoughQuantity error after uptake, %T obtained", err)
+			}
+		}
+		if fixture.uptake <= fixture.quantity && currentQuantity != fixture.quantity-fixture.uptake {
+			t.Errorf(
+				"Expected %d quantity after uptake, obtained %d",
+				fixture.quantity-fixture.uptake,
+				currentQuantity,
+			)
+		}
+	}
+}
